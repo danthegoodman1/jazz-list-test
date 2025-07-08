db6e2cfecc00d4aa66605e10294c7318d2e8ffd6
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-07-07 18:16:25 -0700

    format and public expose

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { JazzReactProvider } from 'jazz-tools/react'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { JazzReactProvider } from "jazz-tools/react"
+import "./index.css"
+import App from "./App.tsx"
 
 // Main wrapper component
 function AppWithJazz() {
   return (
     <JazzReactProvider
-      sync={{ peer: "wss://cloud.jazz.tools/?key=jazz-list-test@demo.app" }}
+      sync={{ peer: "wss://cloud.jazz.tools/?key=test@demotime.app" }}
     >
       <App />
     </JazzReactProvider>
   )
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppWithJazz />
-  </StrictMode>,
+  </StrictMode>
 )
